08c589c400cc841e0a7c777e92e3f2e82c572b92
Raza Chandiwala <rachandiwala@gmail.com>
2015-01-27 15:49:44 -0500

    Senatra: completed Step 2.

--- views/crm.rb
+++ views/crm.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
+require './rolodex'
+require './contact'
 
-get '/' do 
+$rolodex = Rolodex.new    #create a global var
+
+get '/'  do
+  @crm_name = "My CRM"
   erb :index
-  
 end
-  
+
+get "/contacts" do
+  @contacts = $rolodex.contacts
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  contact = Contact.new(params[:id], params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(contact)
+  redirect to('/contacts')
+end
