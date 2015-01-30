94d795a717395148d1514d60f9c1f88b29412f84
Raza Chandiwala <rachandiwala@gmail.com>
2015-01-30 15:50:03 -0500

    CRM Web: HTML and Forms. Find and display contact with Rolodex and Show_contact file

--- crm.rb
+++ crm.rb
@@ -4,6 +4,21 @@ require './contact'   # Include contact.rb also in the the current Dirctory
 
 $rolodex = Rolodex.new    #create a global Class var to have access to Sinatra
 
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "Johnny@bitmakerlabs.com", "Rockstar"))
+
+# A route that coresponds with the above user
+# A route to find and display the contact
+
+get "/contacts/1000"  do
+    @contact = $rolodex.find(1000)
+
+    erb :show_contact
+  end
+
+
+# Above code is newly added code: HTML FORMS
+# Bottom is the old Code from Jan 27 to Jan 29 HTML & CSS
+
 get '/'  do               # root path/URL
   @title = "My CRM Application"    # Create an Instence Var and set it equal to string "my crm"
   erb :index              # Include/look into Views Directory for .erb file called index.erb
@@ -23,3 +38,4 @@ post '/contacts' do
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
+
