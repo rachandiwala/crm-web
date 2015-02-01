67f1da97547ea84f9989fcf2c20a4d50d24ead01
Raza Chandiwala <rachandiwala@gmail.com>
2015-02-01 14:16:05 -0500

    Crm-web: SQLite3, saving data

--- crm.rb
+++ crm.rb
@@ -1,19 +1,35 @@
 require 'sinatra'     # Include Sinatra Library
 require './rolodex'   # Include rolodex.rb, which is in curretn Directory
-require './contact'   # Include contact.rb also in the the current Dirctory
-
-$rolodex = Rolodex.new    #create a global Class var to have access to Sinatra
-
-#$rolodex.add_contact(Contact.new("Johnny", "Bravo", "Johnny@bitmakerlabs.com", "Rockstar"))
+require 'data_mapper' # Translate Ruby code into SQL to talk to database
 
 
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
-get '/'  do               # root path/URL
-  @title = "My CRM Application"    # Create an Instence Var and set it equal to string "my crm"
-  erb :index              # Include/look into Views Directory for .erb file called index.erb
+class Contact
+  Include DataMapper::Resource
+
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize (id, first_name, last_name, email, note)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+
 end
 
-get "/contacts" do        # get/Show path/URL /contacts
+
+$rolodex = Rolodex.new              #create a global Class var to have access to Sinatra
+
+
+get '/'  do                         # root path/URL
+  @title = "My CRM Application"     # Create an Instence Var and set it equal to string "my crm"
+  erb :index                        # Include/look into Views Directory for .erb file called index.erb
+end
+
+get "/contacts" do                  # get/Show path/URL /contacts
   @contacts = $rolodex.contacts     # Create an instence @contacts and set it = to 
   erb :contacts
 end
@@ -30,18 +46,20 @@ end
 
 
 # Above is the old Code from Jan 27 to Jan 29 HTML & CSS
-# Code below code is newly added code: HTML FORMS
+# Code below code is newly added code: HTML & FORMS
 
 
 
-# A route that coresponds with the above user
-# A route to find and display the contact
+# A route to find and display contact by user id, in this case is 1000
 
 get "/contacts/1000"  do
     @contact = $rolodex.find(1000)
 
     erb :show_contact
   end
+
+
+# A route to find and dispaly contact using ids of any user, in this case the :id var stores the users ID
 
 get "/contacts/:id" do
     @contact = $rolodex.find(params[:id].to_i)
@@ -55,6 +73,7 @@ get "/contacts/:id" do
 
 
 # This route is used to display the edit form
+# To access, creat a contact first than goto: localhost/1000/edit
 post "/contacts/:id/edit" do
 
   erb :edit_contact
@@ -69,6 +88,7 @@ get "/contacts/:id/edit" do
       raise Sinatra::NotFound
     end
   end
+
 
 put "/contacts/:id" do
   @contact =$rolodex.find(params[:id].to_i)
@@ -85,6 +105,7 @@ put "/contacts/:id" do
   end
 
 
+# A route to delete a contact/user
   delete "/contacts/:id" do
     @contact = $rolodex.find(params[:id].to_i)
 
